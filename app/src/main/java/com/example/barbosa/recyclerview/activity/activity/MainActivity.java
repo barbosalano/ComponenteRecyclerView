8461d145be50386b47179badf35378aa11027015
barbosalano <personaltecnologiacg@gmail.com>
2018-11-07 10:54:48 -0300

    Eventos de click 2

--- app/src/main/java/com/example/barbosa/recyclerview/activity/activity/MainActivity.java
+++ app/src/main/java/com/example/barbosa/recyclerview/activity/activity/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.barbosa.recyclerview.R;
+import com.example.barbosa.recyclerview.activity.RecyclerItemClickListener;
 import com.example.barbosa.recyclerview.activity.adapter.Adapter;
 import com.example.barbosa.recyclerview.activity.model.Filme;
 
@@ -43,42 +47,71 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
 
         recyclerView.setAdapter(adapter);
+
+        //evento de click
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                Toast.makeText(getApplicationContext(), " Item pressionado: " + filme.getTituloFilme(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get(position);
+                                Toast.makeText(getApplicationContext(), "Click Longo em: " + filme.getTituloFilme() , Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+
+
     }
 
-    public void criarFilmes(){
+    public void criarFilmes() {
 
         Filme filme = new Filme("Mulher Maravilha", "Ação", "2016");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Homem Aranha", "Desenho", "2011");
+        filme = new Filme("Homem Aranha", "Desenho", "2011");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Homem de Ferro", "Animação", "2012");
+        filme = new Filme("Homem de Ferro", "Animação", "2012");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Tarzan", "Aventura", "1990");
+        filme = new Filme("Tarzan", "Aventura", "1990");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Batman", "Desenho", "2014");
+        filme = new Filme("Batman", "Desenho", "2014");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Homem Formiga", "Ação", "2015");
+        filme = new Filme("Homem Formiga", "Ação", "2015");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Super Homem", "Desenho", "2011");
+        filme = new Filme("Super Homem", "Desenho", "2011");
         listaFilmes.add(filme);
 
-        filme = new Filme ("A bela e a fera", "Romance", "2000");
+        filme = new Filme("A bela e a fera", "Romance", "2000");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Carros 2", "Desenho", "2014");
+        filme = new Filme("Carros 2", "Desenho", "2014");
         listaFilmes.add(filme);
 
-        filme = new Filme ("Carros 1", "Desenho", "2015");
+        filme = new Filme("Carros 1", "Desenho", "2015");
         listaFilmes.add(filme);
-        filme = new Filme ("A Múmia", "Terror", "2011");
+        filme = new Filme("A Múmia", "Terror", "2011");
         listaFilmes.add(filme);
-
 
 
     }
